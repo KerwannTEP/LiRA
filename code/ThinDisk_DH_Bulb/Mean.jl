ff2ec258bf4a5948a9966e9eb843ac9072d590cb
KerwannTEP <kerwann_tep@yahoo.fr>
2021-03-02 11:15:09 +0100

    dOmegadxi

--- code/ThinDisk_DH_Bulb/Mean.jl
+++ code/ThinDisk_DH_Bulb/Mean.jl
@@ -13,6 +13,23 @@ function Omega(xi::Float64)
     return ((1-xi)/2)^(3/4)*sqrt(bulb+disk)
 end
 
+function dOmegadxi(xi::Float64)
+    r = a/c
+    xq = x/q
+    num = (-4*q*r^5*sqrt((2-2*xi)/(1-xi+r^2*(1+xi)))
+        + x*((-1+xq^(2/3)*eps)*(1-xi)^(5/2)
+        + r^4*(-1+xq^(2/3)*eps)*sqrt(1-xi)*(1+xi)^2
+        - 2*r^2*(-1+xq^(2/3)*eps)*sqrt(1-xi)*(1-xi^2)
+        + 4*q*r^5*sqrt((2-2*xi)/(1-xi+r^2*(1+xi)))))
+    den = (4*x*(2-2*xi)^(3/4)*(1-xi+r^2*(1+xi))^2
+          *sqrt(1-xq^(2/3)*eps+(2*sqrt(2)*q*r^3*(1-x)
+          /(1+r^2*(1+xi)/(1-xi))^(3/2)))
+          /(x*(1-xi)^(3/2))))
+    return num/den
+end
+
 function alphaSqOverTwoOmega(xi::Float64)
-    return 1 # write expression
+    omega = Omega(xi)
+    domega = dOmegadxi(xi)
+    return 2*omega*(1+(1+xi)*(1-xi)*domega/(2*omega)) 
 end
