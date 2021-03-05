e8f1e863cb3aab738f44bddbf52312590e603d93
KerwannTEP <kerwann_tep@yahoo.fr>
2021-03-05 20:57:24 +0100

    matrix elements

--- code/sources/TableValues.jl
+++ code/sources/TableValues.jl
@@ -9,6 +9,8 @@ const tabIaln = zeros(Float64,2*N+1)
 const tabJln = zeros(Float64,2*N+1,2*N+1)
 const tabLegendre = zeros(Float64,N+1,nbK)
 
+const tabI7ln = zeros(Float64,2*N+1,2*N+1)
+
 # tables which depend on x,q
 tabOmega = zeros(Float64,nbK)
 tabAlphaSqOverTwoOmega = zeros(Float64,nbK)
@@ -20,6 +22,14 @@ tabDln = zeros(Float64,N+1,N+1)
 tabFln = zeros(Float64,N+1,N+1)
 tabGln = zeros(Float64,N+1,N+1)
 tabHln = zeros(Float64,N+1,N+1)
+
+tabAlnt = zeros(Float64,N+1,N+1)
+tabBlnt = zeros(Float64,N+1,N+1)
+tabClnt = zeros(Float64,N+1,N+1)
+tabDlnt = zeros(Float64,N+1,N+1)
+tabFlnt = zeros(Float64,N+1,N+1)
+tabGlnt = zeros(Float64,N+1,N+1)
+tabHlnt = zeros(Float64,N+1,N+1)
 
 # apply in correct order
 
@@ -75,14 +85,67 @@ function tabIln_clear!()
 end
 
 ###############################################
-# Fill Ialm table : Ia(l,m-1) with l=m-1,...,m+2N-1
+# Fill Iln table
 ###############################################
+
+# initialize I(m,m)
+function I7_seed!()
+    prod = 1/(7/4+m+1)
+    for k=1:m
+        prod *= 2*(2*k+1)/(7/4+m+1+k)
+    end
+    tabI7ln[1,1] = prod
+end
+
+# l=m+1,...,m+2N
+function I7_firstLine!()
+    current = tabI7ln[1,1]
+    for l=m+1:m+2*N
+        current *= (l-7/4-m-1)/(l+7/4+m+1)*sqrt(((l+m)*(2*l+1))/((l-m)*(2*l-1)))
+        tabI7ln[l-m+1,1] = current
+        tabI7ln[1,l-m+1] = current #symmetry
+   end
+end
+
+function I7_fill!()
+    for n=m+1:m+N
+        for l=n:m+2*N-(n-m)
+            Ip = (sqrt(((l+m+1)*(l-m+1))/((2*l+1)*(2*l+3)))*tabI7ln[l-m+1+1,n-m+1-1]
+                 + sqrt(((l+m)*(l-m))/((2*l+1)*(2*l-1))) *tabI7ln[l-m+1-1,n-m+1-1])
+            tabI7ln[l-m+1,n-m+1] = sqrt(((2*n+1)*(2*n-1))/((n+m)*(n-m)))*Ip
+            if (n>=m+2)
+                tabI7ln[l-m+1,n-m+1] -= sqrt(((n+m-1)*(n-m-1)*(2*n+1))/((n+m)*(n-m)*(2*n-3)))*tabI7ln[l-m+1,n-m+1-2]
+            end
+            tabI7ln[n-m+1,l-m+1] = tabI7ln[l-m+1,n-m+1] #symmetry
+        end
+    end
+end
+
+function tabI7ln!()
+    I7_seed!()
+    I7_firstLine!()
+    I7_fill!()
+end
+
+function tabI7ln_clear!()
+    for i=1:2*N+1
+        for j=1:2*N+1
+            tabI7ln[i,j] = 0.0
+        end
+    end
+end
+
+###############################################
+# Fill Ialm table : Ia(m-1;l,m-1) with l=m-1,...,m+2N-1
+###############################################
+
+# check the veracity of those expressions
 
 # initialize Ia(m-1,m-1) (a=7/4)
 function Ia_seed!()
-    prod = 1/(7/4+m-1+1)
-    for k=1:m
-        prod *= 2*(2*k+1)/(7/4+m-1+1+k)
+    prod = 1/(7/4+m)
+    for k=1:m-1
+        prod *= 2*(2*k+1)/(7/4+m+k)
     end
     tabIaln[1] = prod
 end
@@ -91,7 +154,7 @@ end
 function Ia_firstLine!()
     current = tabIaln[1]
     for l=m:m+2*N-1
-        current *= (l-7/4-m-1-1)/(l+7/4+m-1+1)*sqrt(((l+m-1)*(2*l+1))/((l-m+1)*(2*l-1)))
+        current *= (l-7/4-m+1-1)/(l+7/4+m-1+1)*sqrt(((l+m-1)*(2*l+1))/((l-m+1)*(2*l-1)))
         tabIaln[l-m+2] = current
    end
 end
@@ -111,23 +174,31 @@ end
 # Fill Jln table
 ###############################################
 
+
+# THIS IS FALSE
+
 # initialize J(m,m) and J(m+1,m)
 function J_seed!()
-    prod = 1
-    for k=1:m
-        prod *= 2*(2*k+1)/(3/4+m+k)
-    end
-    tabJln[1,1] = prod/m
-    prod *= -(7/4)*sqrt(2*m+3)/(3/4+2*m+1)
-    tabJln[2,1] = prod
-    tabJln[2,1] = tabJln[2,1] #symmetry
+    # prod = 1
+    # for k=1:m
+    #     prod *= 2*(2*k+1)/(3/4+m+k)
+    # end
+    # tabJln[1,1] = prod/m # correct
+    # prod *= -(7/4)*sqrt(2*m+3)/(3/4+2*m+1) #false
+    # tabJln[2,1] = prod
+
+     tabJln[1,1] = tabIln[1,1]*(3/4+2*m+1)/(m)
+     tabJln[2,1] = -(7/4)*sqrt(2*m+3)/(3/4+2*m+1)*tabJln[1,1]
+
+
+    tabJln[1,2] = tabJln[2,1] #symmetry
 end
 
 # l=m+1,...,m+2N
 function J_firstLine!()
     for l=m+2:m+2*N
         current = (sqrt(((l-m)*(l-m-1)*(2*l+1))/((l+m)*(l+m-1)*(2*l-3)))*tabJln[l-m+1-2,1]
-                  + sqrt(((2*m+1)*(2*l+1)*(2*l-1))/((2*m)*(l+m)*(l+m-1)))*tabIaln[l-m+2-1])
+                  + 4*sqrt(((2*m+1)*(2*l+1)*(2*l-1))/((2*m)*(l+m)*(l+m-1)))*tabIaln[l-m+2-1])
         tabJln[l-m+1,1] = current
         tabJln[1,l-m+1] = current #symmetry
    end
@@ -223,16 +294,29 @@ end
 # Fill matrix elements table
 ###############################################
 
+# function tabAlnFln!()
+#     for l=m:m+N
+#         for n=l:m+N
+#             integral = 0
+#             for k=1:nbK
+#                 integral += tabLegendre[l-m+1,k]*tabOmega[k]*tabLegendre[n-m+1,k]
+#             end
+#             integral *= 2/nbK
+#             tabAln[l-m+1,n-m+1] = abs(m)*integral
+#             tabFln[l-m+1,n-m+1] = 2*integral
+#             if (n>l)
+#                 tabAln[n-m+1,l-m+1] = tabAln[l-m+1,n-m+1]
+#                 tabFln[n-m+1,l-m+1] = tabFln[l-m+1,n-m+1]
+#             end
+#         end
+#     end
+# end
+
 function tabAlnFln!()
     for l=m:m+N
         for n=l:m+N
-            integral = 0
-            for k=1:nbK
-                integral += tabLegendre[l-m+1,k]*tabOmega[k]*tabLegendre[n-m+1,k]
-            end
-            integral *= 2/nbK
-            tabAln[l-m+1,n-m+1] = abs(m)*integral
-            tabFln[l-m+1,n-m+1] = 2*integral
+            tabAln[l-m+1,n-m+1] = m*sqrt(1-eps)*tabIln[l-m+1,n-m+1]
+            tabFln[l-m+1,n-m+1] = 2*sqrt(1-eps)*tabIln[l-m+1,n-m+1]
             if (n>l)
                 tabAln[n-m+1,l-m+1] = tabAln[l-m+1,n-m+1]
                 tabFln[n-m+1,l-m+1] = tabFln[l-m+1,n-m+1]
@@ -241,11 +325,13 @@ function tabAlnFln!()
     end
 end
 
+# tester ces valeurs
+
 function tabBln!()
     for l=m:m+N
         for n=l:m+N
-            tabBln[l-m+1,n-m+1] = (1/2)*(sqrt(((2*l+1)*(l+m+1)*(l-m+1))/(2*l+3))*tabJln[l-m+1+1,n-m+1]
-                                + tabJln[l-m+1,n-m+1] )
+            tabBln[l-m+1,n-m+1] = ((1/2)*(sqrt(((2*l+1)*(l+m+1)*(l-m+1))/(2*l+3))*tabJln[l-m+1+1,n-m+1]
+                                + tabJln[l-m+1,n-m+1] ))
             if (l>m)
                 tabBln[l-m+1,n-m+1] -= (1/2)*sqrt(((2*l+1)*(l+m)*(l-m))/(2*l-1))*tabJln[l-m+1-1,n-m+1]
             end
@@ -271,10 +357,11 @@ end
 function tabDln!(x::Float64=1.0, q::Float64=1.0)
     for l=m:m+N
         for n=l:m+N
-            tabDln[l-m+1,n-m+1] = (1/2)*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))*(-sqrt(((2*n+1)*(n+m+1)*(n-m+1))/(2*n+3))*tabIln[l-m+1+1,n-m+1]
-                                - tabIln[l-m+1,n-m+1] )
-            if (l>m)
-                tabDln[l-m+1,n-m+1] += (1/2)*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))* sqrt(((2*n+1)*(n+m)*(n-m))/(2*n-1))*tabIln[l-m+1-1,n-m+1]
+            tabDln[l-m+1,n-m+1] = ((1/2)*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))*(-sqrt(((2*n+1)*(n+m+1)*(n-m+1))/(2*n+3))
+                                *tabIln[l-m+1,n-m+1+1]- tabIln[l-m+1,n-m+1] ))
+            if (n>m)
+                tabDln[l-m+1,n-m+1] += ((1/2)*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))* sqrt(((2*n+1)*(n+m)*(n-m))/(2*n-1))
+                                       *tabIln[l-m+1,n-m+1-1])
             end
             if (n>l)
                 tabDln[n-m+1,l-m+1] = tabDln[l-m+1,n-m+1]
@@ -286,7 +373,7 @@ end
 function tabGln!(x::Float64=1.0, q::Float64=1.0)
     for l=m:m+N
         for n=l:m+N
-            tabGln[l-m+1,n-m+1] = (1/2)*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))*tabIln[l-m+1,n-m+1]
+            tabGln[l-m+1,n-m+1] = -m*(1/(2*n+1)-(eps/3)*(q/x)^(2/3))*tabIln[l-m+1,n-m+1]
             if (n>l)
                 tabGln[n-m+1,l-m+1] = tabGln[l-m+1,n-m+1]
             end
@@ -294,15 +381,27 @@ function tabGln!(x::Float64=1.0, q::Float64=1.0)
     end
 end
 
+# function tabHln!()
+#     for l=m:m+N
+#         for n=l:m+N
+#             integral = 0
+#             for k=1:nbK
+#                 integral += tabLegendre[l-m+1,k]*tabAlphaSqOverTwoOmega[k]*tabLegendre[n-m+1,k]
+#             end
+#             integral *= 2/nbK
+#             tabHln[l-m+1,n-m+1] = integral
+#             if (n>l)
+#                 tabHln[n-m+1,l-m+1] = tabHln[l-m+1,n-m+1]
+#             end
+#         end
+#     end
+# end
+
+
 function tabHln!()
     for l=m:m+N
         for n=l:m+N
-            integral = 0
-            for k=1:nbK
-                integral += tabLegendre[l-m+1,k]*tabAlphaSqOverTwoOmega[k]*tabLegendre[n-m+1,k]
-            end
-            integral *= 2/nbK
-            tabHln[l-m+1,n-m+1] = integral
+            tabHln[l-m+1,n-m+1] = (1/2)*sqrt(1-eps)*(tabIln[l-m+1,n-m+1]+3*tabI7ln[l-m+1,n-m+1])
             if (n>l)
                 tabHln[n-m+1,l-m+1] = tabHln[l-m+1,n-m+1]
             end
@@ -310,6 +409,75 @@ function tabHln!()
     end
 end
 
+###############################################
+# Fill matrix elements table numerically integration
+###############################################
+
+function tabAlnFlnt!()
+    for l=m:m+N
+        for n=l:m+N
+            integral = 0
+            for k=1:nbK
+                xik = -1 +(2/nbK)*(k-0.5)
+                xipart = ((1-xik)/2)^(3/4)
+                integral += tabLegendre[l-m+1,k]*xipart*tabLegendre[n-m+1,k]
+            end
+            integral *= 2/nbK
+            tabAlnt[l-m+1,n-m+1] = abs(m)*sqrt(1-eps)*integral
+            tabFlnt[l-m+1,n-m+1] = 2*sqrt(1-eps)*integral
+            if (n>l)
+                tabAlnt[n-m+1,l-m+1] = tabAlnt[l-m+1,n-m+1]
+                tabFlnt[n-m+1,l-m+1] = tabFlnt[l-m+1,n-m+1]
+            end
+        end
+    end
+end
+
+function tabClnt!()
+    for l=m:m+N
+        for n=l:m+N
+            integral = 0
+            for k=1:nbK
+                xik = -1 +(2/nbK)*(k-0.5)
+                xipart = ((1-xik)/2)^(3/4)*((1+xik)/2)^(-1)
+                integral += tabLegendre[l-m+1,k]*xipart*tabLegendre[n-m+1,k]
+            end
+            integral *= 2/nbK
+            tabClnt[l-m+1,n-m+1] = abs(m)*integral
+            if (n>l)
+                tabClnt[n-m+1,l-m+1] = tabClnt[l-m+1,n-m+1]
+            end
+        end
+    end
+end
+
+
+
+function tabHlnt!()
+    for l=m:m+N
+        for n=l:m+N
+            integral = 0
+            for k=1:nbK
+                xik = -1 +(2/nbK)*(k-0.5)
+                xipart1 = ((1-xik)/2)^(3/4)
+                xipart2 = ((1-xik)/2)^(7/4)
+                integral += tabLegendre[l-m+1,k]*(xipart1+3*xipart2)*tabLegendre[n-m+1,k]
+            end
+            integral *= 2/nbK
+            tabHlnt[l-m+1,n-m+1] = (1/2)*sqrt(1-eps)*integral
+            if (n>l)
+                tabHlnt[n-m+1,l-m+1] = tabHlnt[l-m+1,n-m+1]
+            end
+        end
+    end
+end
+
+
+
+
+###############################################
+# Fill matrix elements table
+###############################################
 # do in order
 
 function table_constant_fill!()
@@ -317,6 +485,8 @@ function table_constant_fill!()
     tabIaln!()
     tabJln!()
     tabLegendre!()
+
+    tabI7ln!()
 end
 
 function table_function_fill!(x::Float64=1.0, q::Float64=1.0)
