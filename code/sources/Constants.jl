6f987e1ecbed76cd62fcb7a7c7dd2b76fd1a23f8
KerwannTEP <kerwann_tep@yahoo.fr>
2021-03-30 13:52:45 +0200

    comments

--- code/sources/Constants.jl
+++ code/sources/Constants.jl
@@ -1,8 +1,55 @@
-# z=1.3 N=170
-const a = 1.0 # Disc scale parameter
-const b = 2.8 # Dark halo scale parameter
-const c = 1/20 # Bulge scale parameter
+"""
+    a
+
+Scale length of the Kuzmin-Toomre disk.
+Set to `1.0` by default.
+"""
+const a = 1.0
+
+"""
+    b
+
+Scale length of the Toomre spherical dark halo.
+Set to `2.8` by default, see `Reddish et al. (2021)`.
+"""
+const b = 2.8
+
+"""
+    c
+
+Scale length of the Toomre spherical bulge.
+Set to `1/20` by default, see `Reddish et al. (2021)`.
+"""
+const c = 1/20
+
+"""
+    error_dis
+
+Error threshold on the convergence distance when determining
+physical eigenvalues of the system.
+"""
 const error_dis = 0.005
+
+"""
+    error_var
+
+Error threshold on the convergence variance when determining
+physical eigenvalues of the system.
+"""
 const error_var = 0.005
+
+"""
+    threshold_nb_egv
+
+Minimal number of cutoff matrix eigenvalues that must be found
+to apply the selection criterion.
+"""
 const threshold_nb_egv = 10
-const threshold_GR = 0.1 # Ad-hoc threshold of growth rate
+
+"""
+    threshold_GR
+
+Lower threshold under which we don't compute the growth rate of the system.
+Set to `0.1` by default, see `Reddish et al. (2021)`.
+"""
+const threshold_GR = 0.1
--- code/sources/Main.jl
+++ code/sources/Main.jl
@@ -1,8 +1,66 @@
-include("Packages.jl")
-include("Args.jl")
-include("Constants.jl")
-include("Mean.jl")
-include("TableValues.jl")
-include("ResponseMatrix.jl")
-include("Eigenvalues.jl")
-include("GrowthRate.jl")
+include("Packages.jl") # Import the packages
+include("Args.jl") # Parsing the command-line
+
+##################################################
+# General parameters
+##################################################
+"""
+    PARALLEL
+
+Determining if the code is run in parallel.
+Is 'yes' by default.
+"""
+const PARALLEL = parsed_args["parallel"]
+if ((PARALLEL != "yes") && (PARALLEL != "no"))
+    error("ERROR: UNKNOWN PARALLEL") # Unknown parallel procedure
+end
+
+
+"""
+    eps
+
+Temperature of the disk, defined as the ratio of the internal
+energy over the absolut total energy.
+
+A low temperature means that pressure predominates, while a high
+temperature means that self-gravity predominates.
+
+Is '0.1' by default.
+"""
+const eps = parsed_args["eps"]
+
+"""
+    N
+
+Number of basis elements in the linear expansion.
+
+Is '100' by default.
+"""
+const N = parsed_args["N"]
+
+"""
+    nbK
+
+Sampling number for numerical integration.
+
+Is '5000' by default.
+"""
+const nbK = parsed_args["nbK"]
+
+"""
+    m
+
+Azimutal wavenumber in the Fourier expansion in angles.
+
+Is '2' by default (mode with fastest growth rates).
+"""
+const m = parsed_args["m"]
+
+##################################################
+
+include("Constants.jl") # Physical constants
+include("Mean.jl") # Useful functions of the system
+include("TableValues.jl") # Compute the matrix elements
+include("ResponseMatrix.jl") # Compute the linear response matrix
+include("Eigenvalues.jl") # Computes the system eigenvalues
+include("GrowthRate.jl") # Computes the growth rate of the system
